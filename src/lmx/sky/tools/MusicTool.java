3c35be15110bccc37bc52c73dc92bfd5a13990f8
limuxiao <li19890814@163.com>
2017-09-20 09:31:22 +0800

    add bg music

---
+++ src/lmx/sky/tools/MusicTool.java
@@ -0,0 +1,19 @@
+package lmx.sky.tools;
+
+/**
+ * @classname  MusicTool	--	音乐播放工具类
+ * @desc
+ * @createtime 2017年9月20日 上午9:28:38      
+ * @author liyan
+ *
+ * @version V1.0.1
+ */
+public class MusicTool {
+
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
